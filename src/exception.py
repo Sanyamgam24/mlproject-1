8043956c5eae819eb037689ade4c79e53529b613
Sanyamgam24 <sanyamgambhir123@gmail.com>
2026-06-09 12:22:48 +0530

    change in exception

--- src/exception.py
+++ src/exception.py
@@ -1,23 +1,27 @@
 import sys
 
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb = error_detail.exc_info()
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
 
-    file_name = exc_tb.tb.frame.f_code.co_filename
+    file_name = exc_tb.tb_frame.f_code.co_filename
 
     error_message = (
-        f"error has occured in python script [{file_name}] at line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
-
+        f"Error has occurred in python script [{file_name}] "
+        f"at line number [{exc_tb.tb_lineno}] "
+        f"error message [{str(error)}]"
     )
+
     return error_message
 
+
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self, error_message, error_detail: sys):
         super().__init__(error_message)
+
         self.error_message = error_message_detail(
             error_message,
             error_detail
         )
 
-        def __str__(self):
-            return self.error_message
+    def __str__(self):
+        return self.error_message

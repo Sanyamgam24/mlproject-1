a45e62e7d9442b341e21e069cf9daccf0f429339
Sanyamgam24 <sanyamgambhir123@gmail.com>
2026-06-09 12:21:04 +0530

    second commit

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+# logger.py
+
+import logging
+import os
+from datetime import datetime
+
+# Create logs directory
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+# Log file name with timestamp
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger(__name__)
